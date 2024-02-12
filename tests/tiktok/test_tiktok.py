0babab5b1ba0bc5c96de8e751159732927adbbf3
Catherine Foster <74341106+catfoster9@users.noreply.github.com>
2024-02-11 20:54:47 -0500

    Add files via upload

--- tests/tiktok/test_tiktok.py
+++ tests/tiktok/test_tiktok.py
@@ -2,15 +2,15 @@ from page_objects.PageTiktok import PageTiktok
 import time
 
 class TiktokAudit(PageTiktok):
-    def test_like_hashtag(self):
+    def control_run(self):
         self.fetch_tiktok()
-        self.iterate_through_batches_like_by_hashtag()
+        self.iterate_through_batches()
         time.sleep(10)
-
-'''
+"""
     def test_like_random(self):
         self.fetch_tiktok()
         self.iterate_through_batches_like_random()
         time.sleep(10)
-'''
+        self.chromebrowser.close()
+"""
 
